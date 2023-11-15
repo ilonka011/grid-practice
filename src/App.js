ad8f2f02dbc6dce40630c5ba24273771bbea7ea7
Илона Першина <ilonapershina@Ilonas-Air.lan>
2023-11-14 21:18:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import img1 from './images/arabic-img.webp';
+import img2 from './images/portuguese.webp';
+import img3 from './images/russian-img.jpeg';
+import img4 from './images/chinese-img.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h3>
+        This is My Grid Practice
+      </h3>
+      <div className='container'>
+      <section className='item item-1'>
+        <h4>
+          Arabic
+        </h4>
+        <img src={img1} alt='arabic alphabet'/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          As many people know, Arabic is one of the hardest languages to learn as a non-native speaker. 
+          This language is rich and beautiful in a unique way. One of the letters literally 'smiles' at you. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
+      <section className='item item-2'>
+        <h4>
+          Portuguese
+        </h4>
+        <img src={img2} alt='portuguese words' />
+        <p>
+          What country comes to your mind first when you hear the word 'portuguese'? For me, it always is Brazil. 
+        </p>
+      </section>
+      <section className='item item-3'>
+      <h4>
+        Russian
+      </h4>
+      <img src={img3} alt='russian alphabet' />
+      <p>
+      This languages is also considered extremely difficult. Some of the letters continue being mocked by new learners. 
+      </p>
+      </section>
+      <section className='item item-4'>
+      <h4>
+        Chinese
+      </h4>
+      <img src={img4} alt='chinese characters' />
+      <p>
+        Arguably, the richest and most complex language is Chinese. There are so many characters where each one of them is a seperate piece of art.
+      </p>
+      </section>
+      </div>
     </div>
   );
 }
